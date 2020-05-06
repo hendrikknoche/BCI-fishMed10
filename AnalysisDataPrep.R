dc238dd3ea801fbbb83635aa9563d07ef2a65f68
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-05-06 17:19:59 +0200

    comment

--- AnalysisDataPrep.R
+++ AnalysisDataPrep.R
@@ -8,10 +8,12 @@ pd<-gsheet2tbl('https://docs.google.com/spreadsheets/d/19oiOujc73TEMqXbrQVdUmutt
 #import of log files
 df = readbulk::read_bulk('logFiles', sep=',', na.strings = 'none', stringsAsFactors=FALSE,row.names = NULL)
 col<-ncol(df)
+
 df$PID<-as.factor(substr(df$File,1,2))
 df$condition<-as.factor(substr(df$File,4,5))
 #further rows missing depending on which factors are in the filename
 
+#move important columns up to the front
 df<-df[,c(1,2,(col+1):(col+2),3:col)]
 #the below commmand is not working yet as it depends on what columns there will be in your log files
 # df %>% group_by(PID,condition,)%>%summarize(sumOfTime=sum(timeSinceStart))
