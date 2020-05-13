044fc5c17775c58903c847f203be935fa723ca3f
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-05-13 17:22:57 +0200

    import and analyis stub

--- AnalysisDataPrep.R
+++ AnalysisDataPrep.R
@@ -6,7 +6,7 @@ library(gsheet)
 pd<-gsheet2tbl('https://docs.google.com/spreadsheets/d/19oiOujc73TEMqXbrQVdUmuttcx3M-0MnbajXwKrMxrk/edit?usp=sharing')
 
 #import of log files
-df = readbulk::read_bulk('logFilesData', sep=',', na.strings = 'NA', stringsAsFactors=FALSE,row.names = NULL)
+df = readbulk::read_bulk('cleanedLogFiles', sep=',', na.strings = 'NA', stringsAsFactors=FALSE,row.names = NULL)
 
 # colnames(df)<-c(cols[2:length(cols)-1])
 df$PID<-as.factor(substr(df$File,1,2))
@@ -16,6 +16,7 @@ col<-ncol(df)
 #move important columns up to the front
 df<-df[,c(1,2,col,3:(col-1))]
 #the below commmand is not working yet as it depends on what columns there will be in your log files
-df %>% group_by(PID,InputBlockNo,InputNo,TrialNumber)%>%summarize(sumOfTime=sum(SequenceTime_ms,na.rm = TRUE))%>%view()
-  
+df %>% group_by(PID,InputBlockNo,InputNo,TrialNumber,condition)%>%summarize(sumOfTime=sum(SequenceTime_ms,na.rm = TRUE))%>%view()
+dfs<-df %>% group_by(PID,InputBlockNo,InputNo,TrialNumber,condition)%>%summarize(sumOfTime=sum(SequenceTime_ms,na.rm = TRUE))
+
 
